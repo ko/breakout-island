6e29553b0e2138c50b43a9960bd17ba4af262c83
ko <git@yaksok.net>
2013-10-05 16:23:31 -0700

    Line intersection algorithm added.
    Based on Andre LeMothe's "Tricks of the Windows Game Programming Gurus".

---
+++ src/com/relurori/engine/graphics/shapes/Intersection.java
@@ -0,0 +1,38 @@
+package com.relurori.engine.graphics.shapes;
+
+public class Intersection {
+	
+	private boolean intersect;
+	private float x;
+	private float y;
+	
+	public Intersection() {
+		intersect = false;
+		x = 0;
+		y = 0;
+	}
+	
+	public void setIntersect(boolean bool) {
+		this.intersect = bool;
+	}
+	
+	public boolean getIntersect() {
+		return this.intersect;
+	}
+	
+	public void setX(float x) {
+		this.x = x;
+	}
+	
+	public float getX() {
+		return this.x;
+	}
+	
+	public void setY(float y) {
+		this.y = y;
+	}
+	
+	public float getY() {
+		return this.y;
+	}
+}
--- src/com/relurori/engine/graphics/shapes/Line.java
+++ src/com/relurori/engine/graphics/shapes/Line.java
@@ -29,4 +29,30 @@ public class Line {
 	public float getY2() {
 		return y2;
 	}
+	
+	public Intersection intersectsWith(Line line) {
+		Intersection intersect = new Intersection();
+		float s1x,s1y,s2x,s2y;
+		float s,t;
+		
+		intersect.setIntersect(true);
+		
+		s1x = this.getX2() - this.getX1();
+		s1y = this.getY2() - this.getY1();
+		s2x = line.getX2() - line.getX1();
+		s2y = line.getY2() - line.getY1();
+		
+		s = (-s1y * (this.getX1() - line.getX1()) + s1x * (this.getY1() - line.getY1()));
+		s = s / (-s2x * s1y + s1x * s2y);
+		t = (-s2x * (this.getY1() - line.getY1()) - s2y * (this.getX1() - line.getX1()));
+		t = t / (-s2x * s1y + s1x * s2y);
+		
+		if (s >= 0 && s <= 1 && t >= 0 && t <= 1) {
+			intersect.setIntersect(true);
+			intersect.setX(this.getX1() + (t * s1x));
+			intersect.setY(this.getY1() + (t * s1y));
+		}
+		
+		return intersect;
+	}
 }

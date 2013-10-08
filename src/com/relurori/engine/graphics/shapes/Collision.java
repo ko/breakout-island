f6d5881ccb98f5029d05d41e600f7802452882c6
ko <git@yaksok.net>
2013-10-07 22:41:03 -0700

    Fixes #8 with more natural bouncing.
    Cleaned up some potential Concurrent Array Modifications.

--- src/com/relurori/engine/graphics/shapes/Collision.java
+++ src/com/relurori/engine/graphics/shapes/Collision.java
@@ -8,6 +8,16 @@ public class Collision {
 
 	private static final String TAG = Collision.class.getSimpleName();
 	
+	public static final int INVALID_FACE = 0;
+	public static final int WEST_FACE = 1;
+	public static final int NORTH_FACE = 2;
+	public static final int EAST_FACE = 3;
+	public static final int SOUTH_FACE = 4;
+	public static final int NE_CORNER = 5;
+	public static final int SE_CORNER = 6;
+	public static final int SW_CORNER = 7;
+	public static final int NW_CORNER = 8;
+	
 	private static boolean DEBUG = true;
 
 	public static Intersection between(Circle circle, Rectangle rect) {
@@ -39,11 +49,13 @@ public class Collision {
 				// above
 				Log.d(TAG,"Hit:1");
 				intersection.setIntersect(true);
+				intersection.setFace(Collision.NORTH_FACE);
 			}
 			if (Math.abs(rswy - cy) <= cr) {
 				// below
 				Log.d(TAG,"Hit:2");
 				intersection.setIntersect(true);
+				intersection.setFace(Collision.SOUTH_FACE);
 			}
 		}
 		
@@ -52,11 +64,13 @@ public class Collision {
 				// left
 				Log.d(TAG,"Hit:3");
 				intersection.setIntersect(true);
+				intersection.setFace(Collision.WEST_FACE);
 			}
 			if (Math.abs(rnex - cx) <= cr) {
 				// right
 				Log.d(TAG,"Hit:4");
 				intersection.setIntersect(true);
+				intersection.setFace(Collision.EAST_FACE);
 			}
 		}
 		
